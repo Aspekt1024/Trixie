dd8f37e802f6f55231aaff6992f2a4ce479bf2b9
Dan-PC <d.szabo87@gmail.com>
2017-11-12 19:09:43 +1100

    Created a basic turret that doesn't move, and just shoots mindlessly. Go little guy!

--- Assets/Scripts/Core/Enemies/BasicTurret.cs
+++ Assets/Scripts/Core/Enemies/BasicTurret.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class BasicTurret : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject ProjectilePrefab;
+    public float ProjectileCooldown;
+    public float ProjectileSpeed;
+
+    private float timeLastShot;
+
+    private void Start()
+    {
+        timeLastShot = Time.deltaTime;
+        if (ProjectilePrefab == null)
+        {
+            Debug.LogError("No projectile prefab set for Basic Turret");
+        }
+    }
+
+    private void Update()
+    {
+        if (timeLastShot + ProjectileCooldown < Time.time)
+        {
+            Shoot();
+        }
+    }
+    
+    private void Shoot()
+    {
+        GameObject projectile = ObjectPooler.Instance.GetPooledObject("Projectile");
+        if (projectile == null) return;
+
+        projectile.SetActive(true);
+        projectile.transform.position = transform.position;
+        projectile.transform.localRotation = transform.rotation;
+        projectile.GetComponent<Rigidbody2D>().velocity = transform.up * ProjectileSpeed;
+
+        timeLastShot = Time.time;
+    }
 }
