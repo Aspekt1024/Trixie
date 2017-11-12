a26ead2ffdadc8aee17e46af6b77084dc70fac50
Dan-PC <d.szabo87@gmail.com>
2017-11-12 19:10:00 +1100

    Created an object pool

---
+++ Assets/Scripts/Core/ObjectPooler/ObjectPoolItem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObjectPoolItem {
+
+    public List<GameObject> PooledObjects;
+    public GameObject ObjectPrefab;
+    public int PoolSize;
+    public bool PoolCanGrow;
+
+    private Transform poolItemParent;
+
+    public void Initialise(Transform parentPool)
+    {
+        poolItemParent = new GameObject(ObjectPrefab.name + " Pool").transform;
+        poolItemParent.SetParent(parentPool);
+        
+        PooledObjects = new List<GameObject>();
+        for (int i = 0; i < PoolSize; i++)
+        {
+            AddNewObjectToList();
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < PoolSize; i++)
+        {
+            if (!PooledObjects[i].activeInHierarchy)
+            {
+                return PooledObjects[i];
+            }
+        }
+
+        if (PoolCanGrow)
+        {
+            GameObject obj = AddNewObjectToList();
+            return obj;
+        }
+
+        return null;
+    }
+
+    private GameObject AddNewObjectToList()
+    {
+        GameObject obj = Object.Instantiate(ObjectPrefab, poolItemParent);
+        obj.SetActive(false);
+        PooledObjects.Add(obj);
+        return obj;
+    }
+}
---
+++ Assets/Scripts/Core/ObjectPooler/ObjectPooler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour {
+
+    public static ObjectPooler Instance;
+
+    public List<ObjectPoolItem> ObjectPools;
+
+    private void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("Multiple ObjectPoolers in this scene. Only one can be present");
+            Destroy(gameObject);
+            return;
+        }
+
+        foreach (ObjectPoolItem pool in ObjectPools)
+        {
+            pool.Initialise(transform);
+        }
+    }
+
+    public GameObject GetPooledObject(string tag)
+    {
+        foreach (ObjectPoolItem pool in ObjectPools)
+        {
+            if (pool.ObjectPrefab.tag == tag)
+            {
+                return pool.GetPooledObject();
+            }
+        }
+        return null;
+    }
+}
