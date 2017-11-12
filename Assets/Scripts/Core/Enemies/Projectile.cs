87c48386cd09799ff45b120f65a6b15af79e1039
Dan-PC <d.szabo87@gmail.com>
2017-11-12 21:11:05 +1100

    Projectiles!

---
+++ Assets/Scripts/Core/Enemies/Projectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    private Animator anim;
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void DestroyByCollision()
+    {
+        gameObject.SetActive(false);
+        // TODO hit object animation
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Shield")
+        {
+            // Do nothing. This is handled by the shield component
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            // TODO hit object animation
+        }
+    }
+
+    private void OnEnable()
+    {
+        // TODO set initial state (animations etc)
+    }
+}
