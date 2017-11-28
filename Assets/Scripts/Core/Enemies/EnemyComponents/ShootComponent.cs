0a28d3af98495369379234e52c5435b289adc5be
Dan-PC <d.szabo87@gmail.com>
2017-11-29 00:18:33 +1100

    Added extra stuff for enemy shooting patterns

--- Assets/Scripts/Core/Enemies/EnemyComponents/ShootComponent.cs
+++ Assets/Scripts/Core/Enemies/EnemyComponents/ShootComponent.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class ShootComponent : MonoBehaviour {
 
+    public enum ShootTypes
+    {
+        OneShot, Radial
+    }
+    public ShootTypes ShootType;
+
+    public bool RadialClockwise;
+    public float RadialStartDegree;
+    public float RadialArc;
+    public float RadialDistanceBetweenShots;
+    public float DelayBetweenRadialShots;
+
     public GameObject ProjectilePrefab;
     public GameObject Turrets;
     public float ProjectileCooldown;
@@ -17,6 +29,8 @@ public class ShootComponent : MonoBehaviour {
 
     private Projectile projectilePrefabScript;
     private int layerMask;
+
+    private Coroutine shootCoroutine;
 
     private enum States
     {
@@ -40,12 +54,18 @@ public class ShootComponent : MonoBehaviour {
             case States.None:
                 break;
             case States.FindingTarget:
-                if (TargetInLineOfSight())
+
+                if (ShootType == ShootTypes.Radial)
+                {
+                    state = States.Shooting;
+                }
+                else if (TargetInLineOfSight())
                 {
                     state = States.Aiming;
                 }
                 break;
             case States.Aiming:
+
                 if (Aim())
                 {
                     if (Shoot())
@@ -59,7 +79,7 @@ public class ShootComponent : MonoBehaviour {
                 }
                 break;
             case States.Shooting:
-                state = States.FindingTarget;
+                Shoot();
                 break;
             default:
                 break;
@@ -102,30 +122,81 @@ public class ShootComponent : MonoBehaviour {
     {
         Vector2 distVector = aimTarget.position - transform.position;
         float targetRotation = Mathf.Atan2(distVector.y, distVector.x) * Mathf.Rad2Deg;
-        Turrets.transform.localEulerAngles = new Vector3(0f, 0f, targetRotation);
+        SetLookAngle(targetRotation);
         return true;
+    }
+
+    private void SetLookAngle(float angle)
+    {
+        if (Turrets == null) return;
+        Turrets.transform.localEulerAngles = new Vector3(0f, 0f, angle);
     }
 
     private bool Shoot()
     {
         if (timeLastShot + ProjectileCooldown > Time.time) return false;
-        
+
+        timeLastShot = Time.time;
+
+        if (ShootType == ShootTypes.OneShot)
+        {
+            ShootOneShot();
+        }
+        else
+        {
+            shootCoroutine = StartCoroutine(ShootRadial());
+        }
+        return true;
+    }
+
+    private void ShootOneShot()
+    {
+        ActivateProjecile();
+        state = States.FindingTarget;
+    }
+
+    private IEnumerator ShootRadial()
+    {
+        float angle = RadialStartDegree;
+        float arcCovered = 0f;
+
+        float timeBetweenShots = 0f;
+
+        while(arcCovered <= RadialArc)
+        {
+            SetLookAngle(angle);
+            ActivateProjecile();
+
+            timeBetweenShots = 0f;
+            while (timeBetweenShots < DelayBetweenRadialShots)
+            {
+                timeBetweenShots += Time.deltaTime;
+                yield return null;
+            }
+
+            if (RadialDistanceBetweenShots == 0) break;
+            
+            angle += (RadialClockwise ? -1 : 1) * RadialDistanceBetweenShots;
+            arcCovered += RadialDistanceBetweenShots;
+        }
+    }
+
+    private void ActivateProjecile()
+    {
         GameObject projectile = ObjectPooler.Instance.GetPooledProjectile(projectilePrefabScript.name);
-        if (projectile == null) return false;
+        if (projectile == null) return;
 
         projectile.SetActive(true);
         projectile.transform.position = ShootPoint.transform.position;
         projectile.transform.localRotation = Turrets.transform.rotation;
         projectile.transform.localEulerAngles = new Vector3(0f, 0f, projectile.transform.localEulerAngles.z);
-        projectile.GetComponent<Rigidbody2D>().velocity = Turrets.transform.right * ProjectileSpeed;
+        projectile.GetComponent<Rigidbody2D>().velocity = projectile.transform.right * ProjectileSpeed;
 
         if (projectilePrefabScript.Behaviour == Projectile.ProjectileBehaviours.Homing)
         {
             projectile.GetComponent<Projectile>().SetTarget(aimTarget);
         }
 
-        timeLastShot = Time.time;
-        return true;
     }
 
     private void SetupLayerMask()
---
+++ Assets/Scripts/Editor/Inspectors/ShootComponentInspector.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ShootComponent))]
+public class ShootComponentInspector : Editor {
+
+    private ShootComponent current;
+
+    public override void OnInspectorGUI()
+    {
+        current = (ShootComponent)target;
+
+        ShowCommonUI();
+
+        switch (current.ShootType)
+        {
+            case ShootComponent.ShootTypes.OneShot:
+                ShowOneShotUI();
+                break;
+            case ShootComponent.ShootTypes.Radial:
+                ShowRadialUI();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ShowCommonUI()
+    {
+        current.ShootType = (ShootComponent.ShootTypes)EditorGUILayout.EnumPopup("Shoot Type", current.ShootType);
+        current.ProjectilePrefab = (GameObject)EditorGUILayout.ObjectField("Projectile Prefab", current.ProjectilePrefab, typeof(GameObject), false);
+        current.ProjectileSpeed = EditorGUILayout.FloatField("Projectile Speed", current.ProjectileSpeed);
+        current.ProjectileCooldown = EditorGUILayout.FloatField("Projectile Cooldown", current.ProjectileCooldown);
+        current.VisibleRange = EditorGUILayout.FloatField("Visible Range", current.VisibleRange);
+
+        current.ShootPoint = (Transform)EditorGUILayout.ObjectField("Shoot Point", current.ShootPoint, typeof(GameObject), false);
+        current.Turrets = (GameObject)EditorGUILayout.ObjectField("Turrets", current.Turrets, typeof(GameObject), true);
+
+        for (int i = 0; i < current.TargetLayers.Length; i++)
+        {
+            //current.TargetLayers[i] = EditorGUILayout.
+        }
+
+
+        current.TargetLayers[0] = 1 << LayerMask.NameToLayer("Player");
+
+        //current.TargetLayers = EditorGUILayout.
+
+    }
+
+    private void ShowOneShotUI()
+    {
+    //        
+    //public GameObject Turrets;
+    //public float ProjectileCooldown;
+    //public float ProjectileSpeed;
+    //public float VisibleRange;
+    //public Transform ShootPoint;
+    //public LayerMask[] TargetLayers;
+    }
+
+    private void ShowRadialUI()
+    {
+        EditorGUILayout.Space();
+        current.RadialClockwise = EditorGUILayout.Toggle("Radial Clockwise", current.RadialClockwise);
+
+        current.RadialStartDegree = EditorGUILayout.FloatField("Start Angle", current.RadialStartDegree);
+        current.RadialArc = EditorGUILayout.FloatField("Radial Arc (degrees)", current.RadialArc);
+        current.RadialDistanceBetweenShots = EditorGUILayout.FloatField("Radial Spread", current.RadialDistanceBetweenShots);
+        current.DelayBetweenRadialShots = EditorGUILayout.FloatField("Delay between shots", current.DelayBetweenRadialShots);
+    }
+}
