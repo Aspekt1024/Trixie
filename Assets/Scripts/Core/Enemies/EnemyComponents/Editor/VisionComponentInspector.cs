e852177a93566c3568ac9b8f06e3e811a3366fee
Dan-PC <d.szabo87@gmail.com>
2018-01-04 18:55:07 +1100

    Added Vision Component for enemies

---
+++ Assets/Scripts/Core/Enemies/EnemyComponents/Editor/VisionComponentInspector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(VisionComponent))]
+public class VisionComponentInspector : Editor {
+
+    private VisionComponent current;
+
+    private void OnSceneGUI()
+    {
+        current = (VisionComponent)target;
+
+        if (current.StartingAngle > 180f) current.StartingAngle -= 360f;
+        if (current.StartingAngle < -180f) current.StartingAngle += 360f;
+        current.Arc = Mathf.Clamp(current.Arc, 0, 360f);
+
+        Handles.color = new Color(1f, 0.3f, 0.3f, 0.3f);
+        Handles.DrawSolidArc(current.transform.position, Vector3.forward, Quaternion.AngleAxis(current.StartingAngle, Vector3.forward) * Vector3.right, current.Arc, current.Radius);
+        Handles.color = Color.white;
+    }
+}
---
+++ Assets/Scripts/Core/Enemies/EnemyComponents/VisionComponent.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionComponent : MonoBehaviour {
+
+    public float StartingAngle = 0f;
+    public float Arc = 75f;
+    public float Radius = 10f;
+    public float CheckFrequency = 5f;
+
+    private bool canSeePlayer;
+    private LayerMask layers;
+
+    private enum States
+    {
+        None, Active
+    }
+    private States state;
+
+    private void Start()
+    {
+        layers = 1 << LayerMask.NameToLayer("Terrain") | 1 << LayerMask.NameToLayer("Player");
+    }
+
+    public void Activate()
+    {
+        if (state == States.Active) return;
+        
+        state = States.Active;
+        InvokeRepeating("CheckForPlayer", 0.5f, 1f / CheckFrequency);
+    }
+
+    public void Deactivate()
+    {
+        state = States.None;
+        CancelInvoke();
+    }
+
+    public bool CanSeePlayer() { return canSeePlayer; }
+    
+    private void CheckForPlayer()
+    {
+        Vector2 distVector = (Player.Instance.transform.position - transform.position);
+        if (!IsWithinDistance(distVector) || !IsWithinArc(distVector))
+        {
+            canSeePlayer = false;
+            return;
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, distVector, Radius, layers);
+        if (hit.collider == null)
+        {
+            canSeePlayer = false;
+        }
+        else
+        {
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Terrain"))
+            {
+                canSeePlayer = false;
+            }
+            else
+            {
+                Debug.Log(hit.collider.gameObject.layer);
+                canSeePlayer = true;
+            }
+        }
+    }
+
+    private bool IsWithinDistance(Vector2 distVector)
+    {
+        return distVector.magnitude < Radius;
+    }
+
+    private bool IsWithinArc(Vector2 distVector)
+    {
+        float angle = Mathf.Atan2(distVector.y, distVector.x) * Mathf.Rad2Deg;
+        float minAngle = StartingAngle;
+        float maxAngle = StartingAngle + Arc;
+
+        if (maxAngle > 180f)
+        {
+            maxAngle -= 360f;
+            return (angle > minAngle && angle < 180f) || (angle > -180f && angle < maxAngle);
+        }
+        else
+        {
+            return (angle > minAngle && angle < maxAngle);
+        }
+    }
+}
