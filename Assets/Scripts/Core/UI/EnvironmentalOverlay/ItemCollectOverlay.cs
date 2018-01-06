12544580d19fe6637692c7f832c23b0c8a953982
Dan-PC <d.szabo87@gmail.com>
2018-01-06 14:46:09 +1100

    Created placeholder UI text for collecting major items

--- Assets/Scripts/Core/UI/EnvironmentalOverlay/ItemCollectOverlay.cs
+++ Assets/Scripts/Core/UI/EnvironmentalOverlay/ItemCollectOverlay.cs
@@ -2,30 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ItemCollectOverlay : MonoBehaviour {
 
-    public Text ItemText;   // TODO set this as a prefab so we can collect multiple objects?
+    public TextMeshProUGUI ItemAcquiredText;
+    public TextMeshProUGUI ItemText;
+    public ParticleSystem ItemParticleSystem;
+
+    private Animator anim;
     
 	private void Start ()
     {
-        ItemText.enabled = false;
-	}
+        anim = GetComponent<Animator>();
+        DisableText();
+        ItemParticleSystem.Stop();
+    }
 	
     public void ShowItemText(string itemText)
     {
-        ItemText.text = "Item obtained:\n[" + itemText + "]";
+        ItemText.text = itemText;
         StartCoroutine(ShowItemTextAnimation());
     }
 
     public IEnumerator ShowItemTextAnimation()
     {
-        ItemText.enabled = true;
-
-        ItemText.transform.position = Player.Instance.transform.position + Vector3.up * 2f;
+        EnableText();
+        ItemParticleSystem.Play();
+        anim.Play("Bounce", 0, 0f);
+        
         yield return new WaitForSeconds(3f);
         yield return null;
 
+        DisableText();
+    }
+
+    private void EnableText()
+    {
+        ItemAcquiredText.enabled = true;
+        ItemText.enabled = true;
+    }
+
+    private void DisableText()
+    {
+        ItemAcquiredText.enabled = false;
         ItemText.enabled = false;
     }
 }
