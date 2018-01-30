e6bf35e2acd005c09ce4ff1f3e123fd097e6a1cf
Dan-PC <d.szabo87@gmail.com>
2018-01-31 08:51:23 +1100

    Animations for bulb

---
+++ Assets/Scripts/Core/Environmental/Interactables/Bulb.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulb : MonoBehaviour {
+
+    private Animator anim;
+
+    private enum Animations
+    {
+        Bounce, Idle
+    }
+    
+	private void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+
+    public void Bounce()
+    {
+        anim.Play(Animations.Bounce.ToString(), 0, 0f);
+    }
+}
